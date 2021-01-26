f313d71bb4297f0cf481e54bd82c07901cff0729
Will Adams <bushblade@gmail.com>
2021-01-26 21:37:33 +0000

    fishing

--- config.fish
+++ config.fish
@@ -3,3 +3,24 @@ set -gx EDITOR nvim
 abbr -a r ranger
 abbr -a nv nvim
 abbr -a x exit
+
+# tmux aliases
+alias ta='tmux attach -t'
+alias tad='tmux attach -d -t'
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias tksv='tmux kill-server'
+alias tkss='tmux kill-session -t'
+
+alias ..="cd .."
+alias r="ranger"
+alias x="exit"
+alias nv="nvim"
+alias rm="rm -i"
+alias mv="mv -i"
+alias cl='clear'
+alias ignorenode='attr -s com.dropbox.ignored -V 1 node_modules'
+
+# cleanup orphaned packages
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+
