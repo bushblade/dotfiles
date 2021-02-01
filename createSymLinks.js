1f8dce96338163273763142f0ad1f0799a1a62be
Will Adams <bushblade@gmail.com>
2021-02-01 19:55:52 +0000

    fix naming

--- createSymLinks.js
+++ createSymLinks.js
@@ -6,20 +6,20 @@ const path = require("path");
 const HOME = process.env.HOME;
 
 const links = [
-  { target: "init.vim", path: ".config/nvim/init.vim" },
-  { target: "kitty.conf", path: ".config/kitty/kitty.conf" },
-  { target: "config.fish", path: ".config/fish/config.fish" },
-  { target: "starship.toml", path: ".config/starship.toml" },
-  { target: "tmux.conf", path: ".tmux.conf" },
-  { target: "zshrc", path: ".zshrc" },
-  { target: "coc-settings.json", path: ".config/nvim/coc-settings.json" },
-  { target: "coc/package.json", path: ".config/coc/extensions/package.json" },
-  { target: "rifle.conf", path: ".config/ranger/rifle.conf" },
-  { target: "rc.conf", path: ".config/ranger/rc.conf" },
+  { target: "init.vim", link: ".config/nvim/init.vim" },
+  { target: "kitty.conf", link: ".config/kitty/kitty.conf" },
+  { target: "config.fish", link: ".config/fish/config.fish" },
+  { target: "starship.toml", link: ".config/starship.toml" },
+  { target: "tmux.conf", link: ".tmux.conf" },
+  { target: "zshrc", link: ".zshrc" },
+  { target: "coc-settings.json", link: ".config/nvim/coc-settings.json" },
+  { target: "coc/package.json", link: ".config/coc/extensions/package.json" },
+  { target: "rifle.conf", link: ".config/ranger/rifle.conf" },
+  { target: "rc.conf", link: ".config/ranger/rc.conf" },
 ];
 
-links.forEach(({ target, path }) => {
-  fs.symlinkSync(path.join(HOME, "dotfiles", target), path.join(HOME, path));
+links.forEach(({ target, link }) => {
+  fs.symlinkSync(path.join(HOME, "dotfiles", target), path.join(HOME, link));
 });
 
 console.log("Sym links created!");
