1cd8fa416d32b9a25921d84b6447002c15f3bef5
Will Adams <bushblade@gmail.com>
2021-02-02 17:40:27 +0000

    added alacritty to symlink script

--- createSymLinks.js
+++ createSymLinks.js
@@ -1,49 +1,54 @@
 #!/usr/bin/node
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const HOME = process.env.HOME;
+const HOME = process.env.HOME
 
 const links = [
-  { target: "init.vim", linkPath: ".config/nvim", file: "init.vim" },
-  { target: "kitty.conf", linkPath: ".config/kitty", file: "kitty.conf" },
-  { target: "config.fish", linkPath: ".config/fish", file: "config.fish" },
-  { target: "starship.toml", linkPath: ".config", file: "starship.toml" },
-  { target: "tmux.conf", file: ".tumux.conf" },
-  { target: "zshrc", file: ".zshrc" },
+  { target: 'init.vim', linkPath: '.config/nvim', file: 'init.vim' },
+  { target: 'kitty.conf', linkPath: '.config/kitty', file: 'kitty.conf' },
+  { target: 'config.fish', linkPath: '.config/fish', file: 'config.fish' },
+  { target: 'starship.toml', linkPath: '.config', file: 'starship.toml' },
+  { target: 'tmux.conf', file: '.tumux.conf' },
+  { target: 'zshrc', file: '.zshrc' },
   {
-    target: "coc-settings.json",
-    linkPath: ".config/nvim/",
-    file: "coc-settings.json",
+    target: 'coc-settings.json',
+    linkPath: '.config/nvim/',
+    file: 'coc-settings.json',
   },
   {
-    target: "./coc/package.json",
-    linkPath: ".config/coc/extensions",
-    file: "package.json",
+    target: './coc/package.json',
+    linkPath: '.config/coc/extensions',
+    file: 'package.json',
   },
-  { target: "rifle.conf", linkPath: ".config/ranger", file: "rifle.conf" },
-  { target: "rc.conf", linkPath: ".config/ranger", file: "rc.conf" },
-];
+  { target: 'rifle.conf', linkPath: '.config/ranger', file: 'rifle.conf' },
+  { target: 'rc.conf', linkPath: '.config/ranger', file: 'rc.conf' },
+  {
+    target: 'alacritty.yml',
+    linkPath: '.config/alacritty/alacritty.yml',
+    file: 'alacritty.yml',
+  },
+]
 
 links.forEach(({ target, linkPath, file }) => {
   // check if linkPath exists and create if it doesn't
   if (linkPath && !fs.existsSync(path.join(HOME, linkPath))) {
     fs.mkdirSync(path.join(HOME, linkPath), { recursive: true }, (err) => {
-      if (err) throw err;
-      console.log(`created directory: - ${HOME}/${linkPath}`);
-    });
+      if (err) throw err
+      console.log(`created directory: - ${HOME}/${linkPath}`)
+    })
   }
   const fullFilePath = linkPath
     ? path.join(HOME, linkPath, file)
-    : path.join(HOME, file);
+    : path.join(HOME, file)
 
   // check if file exists and remove if it does
-  const fileExists = fs.existsSync(fullFilePath);
-  if (fileExists) fs.unlinkSync(fullFilePath);
+  const fileExists = fs.existsSync(fullFilePath)
+  if (fileExists) fs.unlinkSync(fullFilePath)
 
   // create the symlink
-  fs.symlinkSync(path.join(HOME, "dotfiles", target), fullFilePath);
-});
+  fs.symlinkSync(path.join(HOME, 'dotfiles', target), fullFilePath)
+})
 
-console.log("Sym links created!");
+console.log('Sym links created!')
