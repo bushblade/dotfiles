6eb61a3d6ec664a9d518142f17d86ad30189b61c
Will Adams <bushblade@gmail.com>
2021-02-02 13:07:36 +0000

    create dirs if not present

--- createSymLinks.js
+++ createSymLinks.js
@@ -6,20 +6,38 @@ const path = require("path");
 const HOME = process.env.HOME;
 
 const links = [
-  { target: "init.vim", link: ".config/nvim/init.vim" },
-  { target: "kitty.conf", link: ".config/kitty/kitty.conf" },
-  { target: "config.fish", link: ".config/fish/config.fish" },
-  { target: "starship.toml", link: ".config/starship.toml" },
-  { target: "tmux.conf", link: ".tmux.conf" },
-  { target: "zshrc", link: ".zshrc" },
-  { target: "coc-settings.json", link: ".config/nvim/coc-settings.json" },
-  { target: "coc/package.json", link: ".config/coc/extensions/package.json" },
-  { target: "rifle.conf", link: ".config/ranger/rifle.conf" },
-  { target: "rc.conf", link: ".config/ranger/rc.conf" },
+  { target: "init.vim", linkPath: ".config/nvim", file: "init.vim" },
+  { target: "kitty.conf", linkPath: ".config/kitty", file: "kitty.conf" },
+  { target: "config.fish", linkPath: ".config/fish", file: "config.fish" },
+  { target: "starship.toml", linkPath: ".config", file: "starship.toml" },
+  { target: "tmux.conf", file: ".tumux.conf" },
+  { target: "zshrc", file: ".zshrc" },
+  {
+    target: "coc-settings.json",
+    linkPath: ".config/nvim/",
+    file: "coc-settings.json",
+  },
+  {
+    target: "./coc/package.json",
+    linkPath: ".config/coc/extensions",
+    file: "package.json",
+  },
+  { target: "rifle.conf", linkPath: ".config/ranger", file: "rifle.conf" },
+  { target: "rc.conf", linkPath: ".config/ranger", file: "rc.conf" },
 ];
 
-links.forEach(({ target, link }) => {
-  fs.symlinkSync(path.join(HOME, "dotfiles", target), path.join(HOME, link));
+links.forEach(({ target, linkPath, file }) => {
+  // check if linkPath exists and create if it doesn't
+  if (!fs.existsSync(path.join(HOME, linkPath))) {
+    fs.mkdirSync(path.join(Home, linkPath), { recursive: true }, (err) => {
+      if (err) throw err;
+      console.log(`created directory: - ${HOME}/${linkPath}`);
+    });
+  }
+  fs.symlinkSync(
+    path.join(HOME, "dotfiles", target),
+    path.join(HOME, linkPath, file)
+  );
 });
 
 console.log("Sym links created!");
