9e61e55d01c98472d349503ef558b5eac62a1641
Will Adams <bushblade@gmail.com>
2023-03-02 17:42:48 +0000

    wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local mux = wezterm.mux
+
+-- maximize on startup
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
+return {
+	color_scheme = "tokyonight",
+	font = wezterm.font("Victor Mono Nerd Font"),
+	window_background_opacity = 0.9,
+	keys = {
+		{ key = "s", mods = "SHIFT|CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "v", mods = "SHIFT|CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "H", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Left") },
+		{ key = "LeftArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "L", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Right") },
+		{ key = "RightArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "K", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Up") },
+		{ key = "UpArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "J", mods = "SHIFT|CTRL", action = act.ActivatePaneDirection("Down") },
+		{ key = "DownArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
+	},
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
+}
