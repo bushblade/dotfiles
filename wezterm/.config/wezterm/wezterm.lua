91546c00911e968d02f268f5536626b57fa2c0b2
Will Adams <bushblade@gmail.com>
2023-03-04 21:08:05 +0000

    tokyonight style wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ end)
 return {
 	color_scheme = "tokyonight",
 	font = wezterm.font("Victor Mono Nerd Font"),
-	window_background_opacity = 0.9,
+	window_background_opacity = 0.98,
 	keys = {
 		{ key = "x", mods = "SHIFT|CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ key = "v", mods = "SHIFT|CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
@@ -44,6 +44,101 @@ return {
 		{
 			regex = "\\bhttp://localhost:(\\d+)\\b",
 			format = "http://localhost:$1",
+		},
+	},
+	window_frame = {
+		-- The font used in the tab bar.
+		-- Roboto Bold is the default; this font is bundled
+		-- with wezterm.
+		-- Whatever font is selected here, it will have the
+		-- main font setting appended to it to pick up any
+		-- fallback fonts you may have used there.
+		font = wezterm.font({ family = "Victor Mono Nerd Font", weight = "Bold" }),
+
+		-- The size of the font in the tab bar.
+		-- Default to 10. on Wwindows but 12.0 on other systems
+		font_size = 11.0,
+
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#15161e",
+
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#15161e",
+	},
+
+	colors = {
+		tab_bar = {
+			-- The color of the strip that goes along the top of the window
+			-- (does not apply when fancy tab bar is in use)
+			background = "#0b0022",
+
+			-- The active tab is the one that has focus in the window
+			active_tab = {
+				-- The color of the background area for the tab
+				bg_color = "#7aa2f7",
+				-- The color of the text for the tab
+				fg_color = "#15161e",
+
+				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+				-- label shown for this tab.
+				-- The default is "Normal"
+				intensity = "Normal",
+
+				-- Specify whether you want "None", "Single" or "Double" underline for
+				-- label shown for this tab.
+				-- The default is "None"
+				underline = "None",
+
+				-- Specify whether you want the text to be italic (true) or not (false)
+				-- for this tab.  The default is false.
+				italic = false,
+
+				-- Specify whether you want the text to be rendered with strikethrough (true)
+				-- or not for this tab.  The default is false.
+				strikethrough = false,
+			},
+
+			-- Inactive tabs are the tabs that do not have focus
+			inactive_tab = {
+				bg_color = "#292e42",
+				fg_color = "#545c7e",
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `inactive_tab`.
+			},
+
+			-- You can configure some alternate styling when the mouse pointer
+			-- moves over inactive tabs
+			inactive_tab_hover = {
+				bg_color = "#33467c",
+				fg_color = "#c0caf5",
+				italic = true,
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `inactive_tab_hover`.
+			},
+
+			-- The new tab button that let you create new tabs
+			new_tab = {
+				bg_color = "#292e42",
+				fg_color = "#545c7e",
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `new_tab`.
+			},
+
+			-- You can configure some alternate styling when the mouse pointer
+			-- moves over the new tab button
+			new_tab_hover = {
+				bg_color = "#1a1b26",
+				fg_color = "#c0caf5",
+				italic = true,
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `new_tab_hover`.
+			},
 		},
 	},
 }
