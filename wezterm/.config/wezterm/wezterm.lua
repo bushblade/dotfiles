06cf733d4f90653362d9c622d8869b90fab4fae7
Will Adams <bushblade@gmail.com>
2023-03-05 22:08:36 +0000

    styling wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -67,7 +67,6 @@ return {
 		-- the window is not focused
 		inactive_titlebar_bg = "#15161e",
 	},
-
 	colors = {
 		tab_bar = {
 			-- The color of the strip that goes along the top of the window
@@ -122,8 +121,8 @@ return {
 
 			-- The new tab button that let you create new tabs
 			new_tab = {
-				bg_color = "#292e42",
-				fg_color = "#545c7e",
+				bg_color = "#1a1b26",
+				fg_color = "#c0caf5",
 
 				-- The same options that were listed under the `active_tab` section above
 				-- can also be used for `new_tab`.
