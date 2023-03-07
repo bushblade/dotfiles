59bfefed802c317477fe6c5a11dc5787ade67591
Will Adams <bushblade@gmail.com>
2023-03-07 21:51:53 +0000

    correct mod key

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,6 +8,39 @@ wezterm.on("gui-startup", function()
 	window:gui_window():maximize()
 end)
 
+local function isViProcess(pane)
+	-- get_foreground_process_name On Linux, macOS and Windows,
+	-- the process can be queried to determine this path. Other operating systems
+	-- (notably, FreeBSD and other unix systems) are not currently supported
+	return pane:get_foreground_process_name():find("n?vim") ~= nil
+	-- return pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+	if isViProcess(pane) then
+		window:perform_action(
+			-- This should match the keybinds you set in Neovim.
+			act.SendKey({ key = vim_direction, mods = "CTRL" }),
+			pane
+		)
+	else
+		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+	end
+end
+
+wezterm.on("ActivatePaneDirection-right", function(window, pane)
+	conditionalActivatePane(window, pane, "Right", "l")
+end)
+wezterm.on("ActivatePaneDirection-left", function(window, pane)
+	conditionalActivatePane(window, pane, "Left", "h")
+end)
+wezterm.on("ActivatePaneDirection-up", function(window, pane)
+	conditionalActivatePane(window, pane, "Up", "k")
+end)
+wezterm.on("ActivatePaneDirection-down", function(window, pane)
+	conditionalActivatePane(window, pane, "Down", "j")
+end)
+
 return {
 	color_scheme = "tokyonight",
 	font = wezterm.font("Victor Mono Nerd Font"),
@@ -15,14 +48,18 @@ return {
 	keys = {
 		{ key = "x", mods = "SHIFT|CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ key = "v", mods = "SHIFT|CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+		-- { key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
 		{ key = "LeftArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+		-- { key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
 		{ key = "RightArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+		-- { key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
 		{ key = "UpArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+		-- { key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
 		{ key = "DownArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
+		{ key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
+		{ key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
+		{ key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
 	},
 	mouse_bindings = {
 		-- Ctrl-click will open the link under the mouse cursor
