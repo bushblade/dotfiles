ede8bab4e7b77cc292d1589b0bea056465b5e933
Will Adams <bushblade@gmail.com>
2024-05-22 19:43:30 +0100

    tinkering with Wezterm again...

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 local mux = wezterm.mux
+local c = wezterm.config_builder()
 
 -- maximize on startup
 wezterm.on("gui-startup", function()
@@ -41,143 +42,87 @@ wezterm.on("ActivatePaneDirection-down", function(window, pane)
 	conditionalActivatePane(window, pane, "Down", "j")
 end)
 
-return {
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font_with_fallback({ "Victor Mono Nerd Font", "Noto Color Emoji" }),
-	hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.95,
-	keys = {
-		{ key = "x", mods = "SHIFT|CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "v", mods = "SHIFT|CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		-- { key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
-		{ key = "LeftArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
-		-- { key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
-		{ key = "RightArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
-		-- { key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
-		{ key = "UpArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
-		-- { key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
-		{ key = "DownArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
-		-- Commented out as interfering with Tmux bindings
-		-- { key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
-		-- { key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
-		-- { key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
-		-- { key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
-	},
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
-	bypass_mouse_reporting_modifiers = "CTRL|SHIFT",
-	hyperlink_rules = {
-		-- Linkify things that look like URLs and the host has a TLD name.
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-			format = "$0",
-		},
+c.color_scheme = "Catppuccin Mocha"
+c.use_fancy_tab_bar = true
+c.font = wezterm.font_with_fallback({ "Victor Mono Nerd Font", "Noto Color Emoji" })
+c.hide_tab_bar_if_only_one_tab = true
+c.window_background_opacity = 0.85
+c.window_decorations = "RESIZE"
+c.leader = { key = " ", mods = "CTRL" }
 
-		-- Linkify http://localhost:<port> links
-		{
-			regex = "\\bhttp://localhost:(\\d+)\\b",
-			format = "http://localhost:$1",
-		},
-	},
-	window_frame = {
-		-- The font used in the tab bar.
-		-- Roboto Bold is the default; this font is bundled
-		-- with wezterm.
-		-- Whatever font is selected here, it will have the
-		-- main font setting appended to it to pick up any
-		-- fallback fonts you may have used there.
-		font = wezterm.font({ family = "Victor Mono Nerd Font", weight = "Bold" }),
+c.keys = {
+	{ key = "s", mods = "SHIFT|CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "SHIFT|CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "LeftArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Left", 1 }) },
+	{ key = "RightArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Right", 1 }) },
+	{ key = "UpArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Up", 1 }) },
+	{ key = "DownArrow", mods = "SHIFT|CTRL", action = act.AdjustPaneSize({ "Down", 1 }) },
+	{ key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
+	{ key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
+	{ key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
+	{ key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
+	{ key = "L", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(1) },
+	{ key = "H", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
+}
 
-		-- The size of the font in the tab bar.
-		-- Default to 10. on Wwindows but 12.0 on other systems
-		font_size = 11.0,
-
-		-- The overall background color of the tab bar when
-		-- the window is focused
-		active_titlebar_bg = "#15161e",
-
-		-- The overall background color of the tab bar when
-		-- the window is not focused
-		inactive_titlebar_bg = "#15161e",
-	},
-	colors = {
-		-- tab_bar = {
-		-- 	-- The color of the strip that goes along the top of the window
-		-- 	-- (does not apply when fancy tab bar is in use)
-		-- 	background = "#0b0022",
-		--
-		-- 	-- The active tab is the one that has focus in the window
-		-- 	active_tab = {
-		-- 		-- The color of the background area for the tab
-		-- 		bg_color = "#7aa2f7",
-		-- 		-- The color of the text for the tab
-		-- 		fg_color = "#15161e",
-		--
-		-- 		-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-		-- 		-- label shown for this tab.
-		-- 		-- The default is "Normal"
-		-- 		intensity = "Normal",
-		--
-		-- 		-- Specify whether you want "None", "Single" or "Double" underline for
-		-- 		-- label shown for this tab.
-		-- 		-- The default is "None"
-		-- 		underline = "None",
-		--
-		-- 		-- Specify whether you want the text to be italic (true) or not (false)
-		-- 		-- for this tab.  The default is false.
-		-- 		italic = false,
-		--
-		-- 		-- Specify whether you want the text to be rendered with strikethrough (true)
-		-- 		-- or not for this tab.  The default is false.
-		-- 		strikethrough = false,
-		-- 	},
-		--
-		-- 	-- Inactive tabs are the tabs that do not have focus
-		-- 	inactive_tab = {
-		-- 		bg_color = "#292e42",
-		-- 		fg_color = "#545c7e",
-		--
-		-- 		-- The same options that were listed under the `active_tab` section above
-		-- 		-- can also be used for `inactive_tab`.
-		-- 	},
-		--
-		-- 	-- You can configure some alternate styling when the mouse pointer
-		-- 	-- moves over inactive tabs
-		-- 	inactive_tab_hover = {
-		-- 		bg_color = "#33467c",
-		-- 		fg_color = "#c0caf5",
-		-- 		italic = true,
-		--
-		-- 		-- The same options that were listed under the `active_tab` section above
-		-- 		-- can also be used for `inactive_tab_hover`.
-		-- 	},
-		--
-		-- 	-- The new tab button that let you create new tabs
-		-- 	new_tab = {
-		-- 		bg_color = "#1a1b26",
-		-- 		fg_color = "#c0caf5",
-		--
-		-- 		-- The same options that were listed under the `active_tab` section above
-		-- 		-- can also be used for `new_tab`.
-		-- 	},
-		--
-		-- 	-- You can configure some alternate styling when the mouse pointer
-		-- 	-- moves over the new tab button
-		-- 	new_tab_hover = {
-		-- 		bg_color = "#1a1b26",
-		-- 		fg_color = "#c0caf5",
-		-- 		italic = true,
-		--
-		-- 		-- The same options that were listed under the `active_tab` section above
-		-- 		-- can also be used for `new_tab_hover`.
-		-- 	},
-		-- },
+c.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.OpenLinkAtMouseCursor,
 	},
 }
+
+c.bypass_mouse_reporting_modifiers = "CTRL|SHIFT"
+
+c.hyperlink_rules = {
+	{
+		regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+		format = "$0",
+	},
+	{
+		regex = "\\bhttp://localhost:(\\d+)\\b",
+		format = "http://localhost:$1",
+	},
+}
+
+c.window_frame = {
+	font = wezterm.font({ family = "Victor Mono Nerd Font", weight = "Bold" }),
+	font_size = 11.0,
+}
+
+-- Tab bar styling plugin
+wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(c, {
+	position = "top",
+	max_width = 32,
+	dividers = "rounded", -- or "slant_left", "arrows", "slant_right", false
+	indicator = {
+		leader = {
+			enabled = true,
+			off = " ",
+			on = " ",
+		},
+		mode = {
+			enabled = true,
+			names = {
+				resize_mode = "RESIZE",
+				copy_mode = "VISUAL",
+				search_mode = "SEARCH",
+			},
+		},
+	},
+	tabs = {
+		numerals = "arabic", -- or "roman"
+		pane_count = "superscript", -- or "subscript", false
+		brackets = {
+			active = { "", ":" },
+			inactive = { "", ":" },
+		},
+	},
+	clock = { -- note that this overrides the whole set_right_status
+		enabled = true,
+		format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
+	},
+})
+
+return c
