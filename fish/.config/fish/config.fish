2de797dbe4b5423c8dd3afb77b87314fee9a1b80
Will Adams <bushblade@gmail.com>
2025-02-01 15:59:46 +0000

    fix: scrolling issue in Neovim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ set fzf_preview_dir_cmd eza --all --color=always
 # Ctrl + Alt + v for fzf files and Ctrl + Alt + h for fzf history
 fzf_configure_bindings --variables=\e\cv --history=\e\ch
 
-abbr -a nv nvim
+abbr -a nv TERM= nvim
 abbr -a x exit
 abbr -a cat bat
 abbr -a find fd
